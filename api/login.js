11b0c3483de673aca774e502bbf0ba2729b23eee
Grennoro <shamilbagauov@gmail.com>
2025-04-12 14:04:50 +0300

    Update login.js

--- api/login.js
+++ api/login.js
@@ -1,8 +1,17 @@
+import fs from 'fs';
+import path from 'path';
+
 export default function handler(req, res) {
   if (req.method === 'POST') {
     const { username, password } = req.body;
 
-    if (username === 'admin' && password === 'admin') {
+    const filePath = path.join(process.cwd(), 'users.json');
+    const fileData = fs.readFileSync(filePath, 'utf-8');
+    const users = JSON.parse(fileData).users;
+
+    const foundUser = users.find(user => user.username === username && user.password === password);
+
+    if (foundUser) {
       res.status(200).json({ success: true });
     } else {
       res.status(401).json({ success: false, message: 'Неверный логин или пароль' });
