fd467cea1225723fd74f5a2d157c6d800819cb71
Grennoro <shamilbagauov@gmail.com>
2025-04-12 14:16:07 +0300

    Update login.js

--- api/login.js
+++ api/login.js
@@ -1,7 +1,8 @@
 import fs from 'fs';
 import path from 'path';
+import bcrypt from 'bcrypt';
 
-export default function handler(req, res) {
+export default async function handler(req, res) {
   if (req.method === 'POST') {
     const { username, password } = req.body;
 
@@ -9,12 +10,17 @@ export default function handler(req, res) {
     const fileData = fs.readFileSync(filePath, 'utf-8');
     const users = JSON.parse(fileData).users;
 
-    const foundUser = users.find(user => user.username === username && user.password === password);
+    const user = users.find(user => user.username === username);
 
-    if (foundUser) {
+    if (!user) {
+      return res.status(401).json({ success: false, message: 'Неверный логин' });
+    }
+
+    const match = await bcrypt.compare(password, user.password);
+    if (match) {
       res.status(200).json({ success: true });
     } else {
-      res.status(401).json({ success: false, message: 'Неверный логин или пароль' });
+      res.status(401).json({ success: false, message: 'Неверный пароль' });
     }
   } else {
     res.status(405).json({ message: 'Метод не разрешён' });
